85381645cced58b1eefbe8daba49ca467e65ec1c
Alejandro Acevedo <ale@doges.dev>
2024-01-10 02:10:42 -0500

    added mob. Basic implementation of mob following the player

---
+++ mob.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+var player
+
+func _ready():
+	player = get_node("/root/Game/Player")
+
+func _physics_process(delta):
+	# calculates the direction to the player
+	var direction = global_position.direction_to(
+		player.global_position
+	)
+	# velocity of the mob
+	velocity = direction * 300.0
+	move_and_slide()
