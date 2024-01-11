ed18350e36969db14a4ee9c1574ff86ce79d829c
Alejandro Acevedo <ale@doges.dev>
2024-01-11 01:48:04 -0500

    mobs explode when shot, added health to the player. Created a basic health bar for the player

--- 2d-survival/player.gd
+++ 2d-survival/player.gd
@@ -1,5 +1,9 @@
 extends CharacterBody2D
 
+signal health_depleted
+
+var health = 100.0
+
 func _physics_process(delta):
 	var direction = Input.get_vector(
 		"move_left","move_right","move_up","move_down"
@@ -13,4 +17,14 @@ func _physics_process(delta):
 		%HappyBoo.play_walk_animation()
 	else:
 		%HappyBoo.play_idle_animation()
-
+	
+	# danage from enemies each second
+	const DAMAGE_RATE = 5.0
+	# detect mobs tht get in touch with player
+	var overlapping_mobs = %HurtBox.get_overlapping_bodies()
+	
+	if overlapping_mobs.size() > 0:
+		health -= DAMAGE_RATE * overlapping_mobs.size() * delta
+		print(health)
+		if health <= 0.0:
+			health_depleted.emit()
