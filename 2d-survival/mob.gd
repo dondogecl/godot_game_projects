4c752ba75c943c0c1eb451d6838f7e4dd5ce973b
Alejandro Acevedo <ale@doges.dev>
2024-01-10 20:06:08 -0500

    mob: added a speed parameter and delta to movement. Exported speed to easily tune-in the value in the inspector

--- 2d-survival/mob.gd
+++ 2d-survival/mob.gd
@@ -2,11 +2,16 @@ extends CharacterBody2D
 
 @onready var player = get_node("/root/Game/Player")
 
+# adjustable speed parameter
+@export var speed = 15000.0
+
 func _physics_process(delta):
 	# calculates the direction to the player
 	var direction = global_position.direction_to(
 		player.global_position
 	)
+	
+	
 	# velocity of the mob
-	velocity = direction * 300.0
+	velocity = direction * speed * delta
 	move_and_slide()
