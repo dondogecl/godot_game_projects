317d0367fbd99f396fa56cc4c7dc62c8e034af2f
Alejandro A <ale@doges.dev>
2024-02-04 03:56:02 -0500

    Player: created custom inputs, mapped keyboard and joystick inputs; changed simple unidirectional Vector3 for basis for thrust

--- Boost/Player.gd
+++ Boost/Player.gd
@@ -5,10 +5,15 @@ extends RigidBody3D
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	# movement (thrust)
-	if Input.is_action_pressed("ui_accept"):
-		position.y += delta
+	if Input.is_action_pressed("boost"):
+		#position.y += delta
+		apply_central_force(basis.y * delta * 1000.0)
+		
 	# rotation control	
-	if Input.is_action_pressed("ui_left"):
-		rotate_z(delta)
-	if Input.is_action_pressed("ui_right"):
-		rotate_z(-delta)
+	if Input.is_action_pressed("rotate_left"):
+		#rotate_z(delta)
+		apply_torque(Vector3(0.0, 0.0, 100.0 * delta))
+		
+	if Input.is_action_pressed("rotate_right"):
+		#rotate_z(-delta)
+		apply_torque(Vector3(0.0, 0.0, -100.0 * delta))
